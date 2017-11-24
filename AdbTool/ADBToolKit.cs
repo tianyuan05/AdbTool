ab29471db4d066541822ea6b3c9952336db48478
Tianzz05 <tianzz05@163.com>
2017-11-24 19:17:33 +0800

    修改程序

--- AdbTool/ADBToolKit.cs
+++ AdbTool/ADBToolKit.cs
@@ -1,6 +1,4 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -8,85 +6,35 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using System.Windows;
 
 namespace AdbTool
 {
-    public class MainWindowViewModel : BindableBase
+    public class ADBToolKit
     {
-
-        #region 字段
-
-        private string command/*="adb shell"*/;
-
-        private string result;
-
-        #endregion
-
-        #region 属性
-
-        public string Command { get => command; set => SetProperty(ref command, value); }
-
-        public string Result { get => result; set => SetProperty(ref result, value); }
-
-        #endregion
-
-
-        #region 构造器
-
-        public MainWindowViewModel()
+        /// <summary>
+        /// 创建一个 Process 实例
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public static Process CreateInstance(string filePath, string parameter)
         {
-            ExcuteCommand = new DelegateCommand(OnExcute);
-            ClearCommand = new DelegateCommand(OnClear);
-        }
-
-
-        #endregion
-        //Process p;
-        #region DelegateCommand
-
-        public DelegateCommand ExcuteCommand { get; private set; }
-
-        public DelegateCommand ClearCommand { get; private set; }
-
-        #endregion
-
-        void OnClear()
-        {
-            Result = string.Empty;
-        }
-
-        void OnExcute()
-        {
-            
-            if (!System.IO.File.Exists(Util.AdbPath))
-            {
-                MessageBox.Show("未找到ADB程序");
-                return;
-            }
             Process p = new Process();
-            p.StartInfo.FileName = Util.AdbPath;           //设定程序名   
-            p.StartInfo.Arguments = $"{command.Trim()}";  //设定程式执行參數   
+            p.StartInfo.FileName = filePath;           //设定程序名   
+            p.StartInfo.Arguments = $"{parameter.Trim()}";  //设定程式执行參數   
             p.StartInfo.UseShellExecute = false;        //关闭Shell的使用   
             p.StartInfo.RedirectStandardInput = true;   //重定向标准输入   
             p.StartInfo.RedirectStandardOutput = true;  //重定向标准输出   
             p.StartInfo.RedirectStandardError = true;   //重定向错误输出   
-            p.StartInfo.CreateNoWindow = true;          //设置不显示窗口   
-            p.Start();
+            p.StartInfo.CreateNoWindow = false;          //设置不显示窗口  
 
-            var rs = GetAdbCommandOutput(p);
-
-            if (!string.IsNullOrWhiteSpace(rs))
-                Result = rs;
-
-            p.Close();
+            return p;
         }
 
 
-        string GetAdbCommandOutput(Process process)
+        public static string ADBCommandOutput(Process process)
         {
             StringBuilder sb = new StringBuilder();
-
             Thread outputThread = new Thread(new ThreadStart(delegate
             {
                 using (StreamReader reader = process.StandardOutput)
@@ -128,20 +76,11 @@ namespace AdbTool
             catch (Exception)
             {
 
-                throw;
             }
             process.WaitForExit();
 
             return sb.ToString();
         }
-
-
-
-        #region 事件
-
-
-
-        #endregion
 
     }
 }
